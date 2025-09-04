7a02904f335405722de294902a0d78614a9fdcf7
Olepeponcio <olepepinsan@gmail.com>
2025-09-04 09:45:48 +0200

    creando esqueleto del programa
    divido la tarea en dos módulos, pero no creo directorios

---
+++ numeros.py
@@ -0,0 +1,23 @@
+# def mi_generador():
+#     for x in range(1,5):
+#         yield x * 10
+#
+# g = mi_generador()
+#
+# print(next(g))
+# print(next(g))
+# print(next(g))
+# print(next(g))
+
+
+"""funcion que pregunta sección a la que se dirige el cliente"""
+def a_que_seccion_va():
+    print("Buenos días, ¿a qué sección va?")
+
+"""funcion genera turno"""
+
+"""funcion genera turno Perfumeria"""
+
+"""funcion genera turno Farmacia"""
+
+"""funcion genera turno Cosméticos"""
---
+++ principal.py
@@ -0,0 +1,7 @@
+"""TURNERO"""
+# Pregunta donde va
+# Genera turno
+# Perfumeria
+# Farmacia
+# Cosmeticos
+# Pregunta a donde va
